ba540fb820e8c8b2b4f2be56ac6add93d1009615
unknown <ntqnhu1324@gmail.com>
2023-10-21 23:57:27 +0700

    add code

--- js/ea.js
+++ js/ea.js
@@ -1,60 +1,10 @@
-var rad = document.myForm.example;
-var prev = null;
-for (var i = 0; i < rad.length; i++) {
-    rad[i].addEventListener('change', function () {
-        (prev) ? console.log(prev.value) : null;
-        if (this !== prev) {
-            prev = this;
-        }
-        if (this.value == 'bt1') {
-            var element1 = document.getElementById("bt1");
-            element1.classList.remove("hidden");
-            element1.classList.add("block");
-
-            var element2 = document.getElementById("bt2");
-            element2.classList.remove("block");
-            element2.classList.add("hidden");
-        }
-        if (this.value == 'bt2') {
-
-            var element1 = document.getElementById("bt1");
-            element1.classList.remove("block");
-            element1.classList.add("hidden");
-
-            var element2 = document.getElementById("bt2");
-            element2.classList.remove("hidden");
-            element2.classList.add("block");
-
-        }
-    });
+const maxGen = 5
+function convertNumber(num) {
+    return isNaN(num) ? 0 : num
 }
 
-function onSubmitBT1() {
-    var target = document.getElementById('strTarget').value;
-    var n_pop = document.getElementById('N').value;
-    var n_gen = document.getElementById('DB').value;
-
-
-    if (n_pop < 1) {
-        document.getElementById("msg-error1").innerHTML = 'Cá thể ban đầu phải > 0';
-    }
-    else if (n_gen < n_pop) {
-        document.getElementById("msg-error1").innerHTML = 'Số lượng cá thể mới phải > Cá thể ban đầu';
-    } else {
-        document.getElementById("msg-error1").innerHTML = ''
-        onSubmitSearch(target, n_pop, n_gen)
-    }
-}
-
-function checkValidate() {
+function checkValidate(knapsackValue, countItem, beginN, newN, maxValue) {
     let rs = true
-    var knapsackValue = document.getElementById('knapsackValue').value;
-    var countItem = document.getElementById('countItem').value;
-    var beginN = document.getElementById('beginN').value;
-    var newN = document.getElementById('newN').value;
-    var maxValue = document.getElementById('maxValue').value;
-    console.log("checkValidate:knapsackValue, countItem, beginN, newN, maxValue :", knapsackValue, countItem, beginN, newN, maxValue);
-
     if (countItem > 20) {
         document.getElementById("msg-error").innerHTML = 'Số lượng đồ vật không được > 20';
         rs = false
@@ -75,34 +25,54 @@ function checkValidate() {
         document.getElementById("msg-error").innerHTML = 'Cá thể ban đầu không hợp lệ';
         rs = false
     }
-    else if (newN < beginN) {
-        document.getElementById("msg-error").innerHTML = 'Số lượng cá thể mới phải > Cá thể ban đầu';
+    else if (newN < 1) {
+        document.getElementById("msg-error").innerHTML = 'Số lượng thế hệ phải > 0 ';
         rs = false
     }
-
-
+    else if (newN > maxGen) {
+        document.getElementById("msg-error").innerHTML = 'Số lượng thế hệ phải < ' + maxGen;
+        rs = false
+    }
     return rs
+}
+function clearHTML() {
+    let id = ''
+    let myDiv
+    for (let i = 0; i < maxGen; i++) {
+        id = 'newparents' + i
+        myDiv = document.getElementById(id);
+        if (myDiv) {
+            myDiv.innerHTML = ''
+        }
+    }
+
+    Div = document.getElementById("parents");
+    if (myDiv) {
+        myDiv.innerHTML = ''
+    }
 }
 
 
 function onSubmitBT2() {
-    var knapsackValue = document.getElementById('knapsackValue').value;
-    var countItem = document.getElementById('countItem').value;
-    var maxValue = document.getElementById('maxValue').value;
-    var beginN = document.getElementById('beginN').value;
-    var newN = document.getElementById('newN').value;
-    let rsCheck = checkValidate()
+    var knapsackValue = parseInt(document.getElementById('knapsackValue').value);
+    knapsackValue = isNaN(knapsackValue) ? 0 : knapsackValue
+    var countItem = parseInt(document.getElementById('countItem').value);
+    countItem = isNaN(countItem) ? 0 : countItem
+    var beginN = parseInt(document.getElementById('beginN').value);
+    beginN = isNaN(beginN) ? 0 : beginN
+    var newN = parseInt(document.getElementById('newN').value);
+    newN = isNaN(newN) ? 0 : newN
+    var maxValue = parseInt(document.getElementById('maxValue').value);
+    maxValue = isNaN(maxValue) ? 0 : maxValue
+
+    let rsCheck = checkValidate(knapsackValue, countItem, beginN, newN, maxValue)
+    // console.log("hihi", rsCheck)
 
     if (rsCheck) {
         document.getElementById("msg-error").innerHTML = ''
         let strHTML = ''
         let value = 0
         let arrWeight = []
-        // for (let i = 0; i < countItem; i++) {
-        //     value = Math.floor(Math.random() * (maxValue - 1 + 1)) + 1
-        //     strHTML += "  <div class='item'><p>" + value + "</p></div> "
-        //     arrWeight.push(value)
-        // }
 
         while (arrWeight.length !== countItem) {
             value = Math.floor(Math.random() * (maxValue - 1 + 1)) + 1
@@ -111,8 +81,8 @@ function onSubmitBT2() {
                 arrWeight.push(value)
             }
         }
-        // console.log("arrItem", arrItem);
         document.getElementById("list-item").innerHTML = strHTML;
+        document.getElementById("process").innerHTML = '';
         onSubmitKnapsack(beginN, knapsackValue, arrWeight, newN)
     }
 }
--- js/knapsack.js
+++ js/knapsack.js
@@ -1,5 +1,27 @@
-//https://github.com/Pantzan/KnapsackGA/blob/master/knapsack.py
-//https://www.codeconvert.ai/python-to-javascript-converter
+function genFE(arrWeight, arr, idName, index, text) {
+    let id = idName + index
+    let strHTML = ''
+    let value
+    strHTML += "  <h4>" + text + "</h4>"
+    strHTML += "  <div id='" + id + "'   class='" + idName + "'>"
+    for (let i = 0; i < arr.length; i++) {
+        strHTML += "  <div class='item-bag'>"
+        strHTML += "<h5>" + arr[i][1] + "</h5>"
+        for (let j = 0; j < arr[i][0].length; j++) {
+            value = arr[i][0][j]
+            if (value == 1) strHTML += "  <div class='item'><p>" + arrWeight[j] + "</p></div> "
+            else strHTML += "  <div class='item-non'><p>" + arrWeight[j] + "</p></div> "
+        }
+        strHTML += "</div>"
+    }
+    strHTML += "</div>"
+    document.getElementById("process").innerHTML += strHTML;
+
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // hàm mục tiêu
 function fitness(item, knapsackValue, arrWeight) {
@@ -21,21 +43,29 @@ function fitness(item, knapsackValue, arrWeight) {
 }
 //Toán tử đột biến
 function mutation(ch) {
-    for (let i = 0; i < ch.length; i++) {
-        const k = Math.random();
-        if (k > 0.5) {
-            if (ch[i] === 1) {
-                ch[i] = 0;
-            } else {
-                ch[i] = 1;
-            }
-        }
-    }
+    //đột biến ngẫu nhiên nhiều vị trí
+    // for (let i = 0; i < ch.length; i++) {
+    //     const k = Math.random();
+    //     if (k > 0.5) {
+    //         if (ch[i] === 1) {
+    //             ch[i] = 0;
+    //         } else {
+    //             ch[i] = 1;
+    //         }
+    //     }
+    // }
+
+    //đột biến ngẫu nhiên 1 vị trí
+    let position = getRandomNumber(0, ch.length - 1);
+    let value = getRandomNumber(0, 1);
+    ch[position] = value;
+
     return ch;
 }
 //lai ghép hai bố mẹ để sinh ra hai con bằng cách trộn chúng theo tỷ lệ ngẫu nhiên mỗi lần
 function crossover(ch1, ch2) {
     const threshold = Math.floor(Math.random() * (ch1.length - 1)) + 1;
+    // const threshold = Math.floor(ch1.length / 2)
     const tmp1 = ch1.slice(threshold);
     const tmp2 = ch2.slice(threshold);
     ch1 = ch1.slice(0, threshold);
@@ -89,17 +119,18 @@ function onSubmitKnapsack(N, knapsackValue, arrWeight, n_gen) {
 
     //sắp xếp theo thứ tự giảm dần của điểm thích nghi
     parents.sort((a, b) => b[1] - a[1]);
-    // console.log("parents", parents);
+    genFE(arrWeight, parents, "parents", 0, "Quần thể ban đầu")
+
     //chọn cá thể tốt nhất để lai ghép
     let best_parents = parents.filter((item) => item[1] > -1);
     bests = best_parents[0]
     let pop = best_parents.length - 1
     let score1 = 0
     let score2 = 0
-    let gen = 0
+    let gen = 1
     let num = 0
     // console.log("bests", bests);
-    while (newparents.length < n_gen && pop > 0) {
+    while (gen <= n_gen && pop > 0) {
         for (let i = 0; i < pop; i++) {
             if (i < pop - 1) {
                 let r1 = best_parents[i][0];
@@ -147,16 +178,17 @@ function onSubmitKnapsack(N, knapsackValue, arrWeight, n_gen) {
                     num = gen
                 }
             }
-            gen++
         }
         //sắp xếp theo thứ tự giảm dần của điểm thích nghi
         newparents.sort((a, b) => b[1] - a[1]);
+        genFE(arrWeight, newparents, "newparents", gen - 1, "Quần thể thế hệ thứ " + gen + " - số lượng:" + newparents.length)
+        console.log("gen:", gen, " ,best_parents: ", best_parents.length, " ,newparents: ", newparents.length);
 
-        // // Print the progress
-        // if (bests) console.log(`Generation, Best score ${bests[1]}, Best string ${bests[0]}`);
-        pop = newparents.length - 1
-        best_parents = newparents
+        best_parents = newparents.filter((item) => item[1] > -1);
+        pop = best_parents.length - 1
+        gen++
     }
+
     if (bests) {
         let strHTML = "<h2> Danh sách các vật có thể bỏ vào túi là:</h2> <div class='list-item'>"
         for (let index in bests[0]) {
@@ -173,7 +205,6 @@ function onSubmitKnapsack(N, knapsackValue, arrWeight, n_gen) {
     } else {
         document.getElementById("result").innerHTML = "<h2>Không tìm thấy danh sách các vật thỏa điều kiện trên</h2>";
     }
-    // console.log("gen", gen);
 }
 
 
